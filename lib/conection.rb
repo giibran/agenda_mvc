53ef5482a05fdbf0d07771e476f61b8de855b150
freshout <freshout@freshouts-iMac-2.local>
2013-02-22 19:35:22 -0600

    cambios

---
+++ lib/conection.rb
@@ -0,0 +1,10 @@
+require 'mysql2'
+require 'sqlite3-ruby'
+
+class Data_base() 
+
+	def send_query (query)
+		client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "", :database => "agenda")
+		result = client.query(query)
+	end
+end
---
+++ model/person.rb
@@ -0,0 +1,31 @@
+class Person
+	attr_accessor :name, :last_name, :phone, :addresses
+
+	def initialize(id, name, lastname, phone, address)
+    @id = id
+    @name = name
+    @lastname = lastname
+    @phone = phone
+    @address = address
+  end
+
+  def save()
+    	
+  end
+
+  def create()
+    	
+  end
+
+  def find(id)
+    	
+  end
+
+  def destroy()
+    	
+  end  
+
+  def update_attributes()
+    	
+  end
+end
